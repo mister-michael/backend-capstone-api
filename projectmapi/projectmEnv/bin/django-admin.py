a97fa095af3b62b33640f39d34c3fa1858d9cab6
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-11 14:11:17 -0500

    django app created

---
+++ projectmapi/projectmEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/d.a.n.k.e./workspace/backEnd/capstone/backend-capstone-api/projectmapi/projectmEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
