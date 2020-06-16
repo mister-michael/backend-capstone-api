14af89c3133d10e7a3bb06ae65e674d7821dc4e7
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-16 13:05:48 -0500

    phtoshootequipment view full CRUDL

--- projectmapi/backendapi/views/photoshootequipment.py
+++ projectmapi/backendapi/views/photoshootequipment.py
@@ -32,3 +32,57 @@ class PhotoshootEquipments(ViewSet):
         )
 
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            pse = PhotoshootEquipment.objects.get(pk=pk)
+            serializer = PhotoshootEquipmentSerializer(
+                pse, context={'request': request}
+            )
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        
+        pse = PhotoshootEquipment()
+
+        pse.equipment_id = request.data['equipment_id']
+        pse.photoshoot_id = request.data['photoshoot_id']
+
+        pse.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        try:
+            pse = PhotoshootEquipment.objects.get(pk=pk)
+            pse.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except PhotoshootEquipment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+
+        pse = PhotoshootEquipment.objects.all()
+
+        ps_id = self.request.query_params.get('photoshoot_id')
+
+        eq_id = self.request.query_params.get('equipment_id')
+
+        if ps_id is not None:
+            pse = PhotoshootEquipment.objects.filter(photoshoot_id=ps_id)
+
+        if eq_id is not None:
+            pse = PhotoshootEquipment.objects.filter(equipment_id=eq_id)
+
+        serializer = PhotoshootEquipmentSerializer(
+            pse, many=True, context={'request': request}
+        )
+
+        return Response(serializer.data)
